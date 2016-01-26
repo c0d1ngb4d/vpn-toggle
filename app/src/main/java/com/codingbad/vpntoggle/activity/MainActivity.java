6260a38d3478ae9506f0ff5ceb8009e1dad1cc8f
Joaquin Rinaudo <jmrinaudo@gmail.com>
2016-01-26 13:57:39 -0300

    if items null, return empty

--- app/src/main/java/com/codingbad/vpntoggle/activity/MainActivity.java
+++ app/src/main/java/com/codingbad/vpntoggle/activity/MainActivity.java
@@ -41,6 +41,8 @@ import com.codingbad.vpntoggle.model.ListOfApplicationItems;
 import com.codingbad.vpntoggle.service.NetworkManagerIntentService;
 
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AbstractSideBarActivity implements ApplicationsListFragment.Callbacks {
 
